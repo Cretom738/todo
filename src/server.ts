eaf314ee29a0523aa8dc45359acc39e556bf30b2
Cretom738 <CretomPlay738@gmail.com>
2024-03-16 17:38:09 +0500

    init

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`app is running on ${process.env.ENV} and listening on port ${port}`);
+});
